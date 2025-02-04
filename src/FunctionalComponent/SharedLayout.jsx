81bb7eab2f15f1c12801d357c3f8d2d5d6856880
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-02-04 06:26:05 -0800

    update sharedLayout

--- src/FunctionalComponent/SharedLayout.jsx
+++ src/FunctionalComponent/SharedLayout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from './header';
+import Header from './Header';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 const SharedLayout = () => {
     return (
         <div>
-            <Header/>
-            <Outlet/>
-            <Footer/>
+            <Header />
+            <Outlet />
+            <Footer />
         </div>
     );
 }
